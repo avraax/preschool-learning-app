c190a9d439ec4421403aaa648078df90835adce4
Allan Brink Vraa <allanvraa@gmail.com>
2025-07-25 20:12:07 +0200

    Update build info

--- src/config/version.ts
+++ src/config/version.ts
@@ -2,7 +2,7 @@
 // This file is updated automatically during the build process
 
 export const BUILD_INFO = {
-  buildTime: 1753466630232,
+  buildTime: 1753466799386,
   version: '1.0.0',
   commitHash: 'dev'
 }
